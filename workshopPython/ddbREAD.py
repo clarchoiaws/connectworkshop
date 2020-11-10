c708d8262d55f2489ca96de7b6e955cae31acf62
Clarence Choi <clarchoi@amazon.com>
2020-11-10 00:01:30 -0500

    Update ddbREAD.py

--- workshopPython/ddbREAD.py
+++ workshopPython/ddbREAD.py
@@ -7,7 +7,7 @@ def lambda_handler(event, context):
     CallerID = event['Details']['Parameters']['CallerID']
 
     dynamodb = boto3.resource('dynamodb')
-    table = dynamodb.Table('AmazonConnectLab1001Table')
+    table = dynamodb.Table('CustomerTable')
 
     response = table.get_item(
             Key = { 'CallerID': CallerID }
