bc3e9e96e7ed5ed7283c212101b49b85c664958c
EC2 Default User <ec2-user@ip-172-31-7-223.ec2.internal>
2021-01-31 18:11:35 +0000

    adding timezone as a parameter

--- holidaycheck/lambda_function.py
+++ holidaycheck/lambda_function.py
@@ -3,12 +3,21 @@ import dateutil.tz
 
 def lambda_handler(event, context):
 
-    eastern = dateutil.tz.gettz('US/Eastern')
+    # Timezone default to US/Eastern if it is not passed in through Parameters
+    
+    param = event['Details']['Parameters']
+
+    if "timezone" in param:
+        timezone = param['timezone']
+    else:
+        timezone = 'US/Eastern'
+
+    eastern = dateutil.tz.gettz(timezone)
 
     d1 = datetime.datetime.now(tz=eastern) 
     
     today = str(d1.date())
-    print (today)
+    print ('today is ', today)
     with open('holidays.txt') as datafile:
         if today in datafile.read():
             return {"holiday": "True"}
